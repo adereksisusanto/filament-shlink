e592feba8673045db023f154359e87fdacd78058
adereksisusanto <34746706+adereksisusanto@users.noreply.github.com>
2026-07-06 06:54:26 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Adereksisusanto\FilamentShlink\Tests;
 
+use Adereksisusanto\FilamentShlink\FilamentShlinkServiceProvider;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
@@ -19,7 +20,6 @@ use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Adereksisusanto\FilamentShlink\FilamentShlinkServiceProvider;
 
 class TestCase extends Orchestra
 {
